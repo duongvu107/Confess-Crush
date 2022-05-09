8f4db17bad9118cb818454dbac644dba7885b5c8
duongvu107 <105214814+duongvu107@users.noreply.github.com>
2022-05-09 19:21:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
-    titleWeb: "Gửi người con gái đang đi loanh quanh trong đàu em <3",
+    titleWeb: "Gửi người con gái em yêu <3",
     introTitle: 'Vũ Diệp Anh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Bé oi 🥰',
+    desc: 'Bé ew em hông zọ  ',
+    btnYes: 'Yêu lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bé nại yêu em nhở <3',
     btnReply: 'Gửi cho bạn <3',
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Xíu call học như qua nha:33',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/profile.php?id=100040997793892' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
